dca6e932b89a565f8e18851bab1b1fe5a3c3b7cb
Saimir avdija <katundari_@hotmail.com>
2020-10-07 20:11:06 +0200

    Add project files.

---
+++ EmployeeManagement/Models/MockEmployeeRepository.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Server.IIS.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Models
+{
+    public class MockEmployeeRepository : IEmployeeRepository
+    {
+        private List<Employee> _employeeList;
+
+        public MockEmployeeRepository()
+        {
+            _employeeList = new List<Employee>()
+            {
+                new Employee() {Id = 1, Name = "Mary", Department = "HR", Email = "mary@gmail.com"},
+                new Employee() {Id = 2, Name = "John", Department = "IT", Email = "john@gmail.com"},
+                new Employee() {Id = 3, Name = "Sam", Department = "IT", Email = "Sam@gmail.com"}
+            };
+        }
+
+        public IEnumerable<Employee> GetAllEmployees()
+        {
+            return _employeeList;
+        }
+
+        public Employee GetEmployee(int id)
+        {
+            return _employeeList.FirstOrDefault(e => e.Id == id);
+        }
+    }
+}
